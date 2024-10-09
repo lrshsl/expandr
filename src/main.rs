787ed2ad8bf187b5d2cac6072359ef148b341c08
lrshsl <hoeslilars@gmail.com>
2024-10-10 01:50:39 +0200

    Add basic parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::fs::read_to_string;
+
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct ExrParser;
+
 fn main() {
-    println!("Hello, world!");
+    let file_contents = read_to_string("examples/example.exr").unwrap();
+    let parse_result = ExrParser::parse(Rule::prog, &file_contents);
+    println!("{parse_result:#?}")
 }
