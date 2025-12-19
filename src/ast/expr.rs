03002099e7c4147b2a991d09d32a0ba4f9bea5cf
lrshsl <hoeslilars@gmail.com>
2025-12-19 23:40:43 +0100

    Automatic lookup of identifiers used as expressions
    If an identifier is used where an expression is expected, automatically
    treat it as an expression: `[a b]` -> `[a [b]]` if `a` expects an
    expression as argument

--- src/ast/expr.rs
+++ src/ast/expr.rs
@@ -67,7 +67,17 @@ impl<S: SourceType> Expandable for Expr<S> {
             Expr::TemplateString(tmpl_string) => tmpl_string.expand(ctx),
             Expr::Integer(val) => Ok(Int(val)),
 
-            Expr::PathIdent(_) => unimplemented!(),
+            Expr::PathIdent(ident) => {
+                // This branch is called when an argument is an Ident although the mapping expects
+                // an expression. The ident is treated as an expression in this case (mapping
+                // application without arguments), to allow writing things like `[m x + y]` instead
+                // of having to write `[m [x] + [y]]` explicitly.
+                let pseudo_mapping: MappingApplication<S> = MappingApplication {
+                    path_ident: ident,
+                    args: vec![],
+                };
+                pseudo_mapping.expand(ctx)
+            }
             Expr::LiteralSymbol(s) => {
                 unreachable!("Should not try to expand a literal symbol: {s}")
             }
